8e73899980dd701a2a22616a45c0df34198220b6
Miguel Angel Martinez <tartanasdev@gmail.com>
2024-10-21 20:11:58 +0200

    Estructura base creada

---
+++ php/App/Services/Service.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Joc4enRatlla\Services;
+
+class Service
+{
+    public static function loadView($view, $data = [])
+    {
+        $viewPath = str_replace('.', '/', $view);
+        extract($data);
+
+        include  $_SERVER['DOCUMENT_ROOT'] . "/../Views/$viewPath.view.php";
+
+    }
+}
+?>
